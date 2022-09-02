e2903ca4b7f6947bcbd1be9a13cf2c887fa1c1b6
Gean Anjos <ganjos@kornerstonecredit.com>
2022-09-01 21:21:54 -0300

    Exemplo do Factory Method Pattern

---
+++ FactoryMethod/FactoryMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using FactoryMethod;
+
+var fm = new FactoryMethodClass();
+
+Console.WriteLine("Liu Kang | SubZero | Scorpion");
+Console.WriteLine();
+
+Console.WriteLine("Escolha seu personagem");
+string escolha = Console.ReadLine();
+
+IPersonagem personagem = fm.EscolherPersonagem(escolha);
+
+Console.WriteLine();
+Console.Write("Voce vai jogar com ");
+personagem.Escolhido();
+
+Console.ReadKey();
