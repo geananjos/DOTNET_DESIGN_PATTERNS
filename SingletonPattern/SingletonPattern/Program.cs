37cee5565cc9414d4762e0ec405a479926329207
Gean Anjos <ganjos@kornerstonecredit.com>
2022-09-08 22:41:25 -0300

    Exemplo Singleton Pattern

--- SingletonPattern/SingletonPattern/Program.cs
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using SingletonPattern;
+
+Singleton jogador1 = Singleton.GetInstancia;
+jogador1.Mensagem("Jogador 1: A bola esta comigo no meio do campo.");
+
+Singleton jogador2 = Singleton.GetInstancia;
+jogador2.Mensagem("Jogador 2: Recebeu a bola.");
+
+Singleton jogador3 = Singleton.GetInstancia;
+jogador3.Mensagem("Jogador 3: Recebeu o lancamento na linha de fundo.");
+
+Console.ReadKey();
