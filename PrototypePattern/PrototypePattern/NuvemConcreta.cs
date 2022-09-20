bd6f22e8850a5f4f535a6e506be5f19e08228220
Gean Anjos <ganjos@kornerstonecredit.com>
2022-09-19 21:59:20 -0300

    Exemplo do Prototype Pattern

---
+++ PrototypePattern/PrototypePattern/NuvemConcreta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    public class NuvemConcreta : NuvemMolde
+    {
+        private string cor_preenchimento { get; set; }
+        private string cor_contorno { get; set; }
+
+        public NuvemConcreta(string preenchimento, string contorno)
+        {
+            this.cor_preenchimento = preenchimento;
+            this.cor_contorno = contorno;
+        }
+
+        public override NuvemMolde Clone()
+        {
+            Console.WriteLine($@"A nuvem clonada tem contorno {this.cor_contorno} e preenchimento {this.cor_preenchimento}");
+            return this.MemberwiseClone() as NuvemMolde;
+        }
+    }
+}
--- PrototypePattern/PrototypePattern/Program.cs
+++ PrototypePattern/PrototypePattern/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using PrototypePattern;
+
+GerenciadorNuvens gerenciadorNuves = new GerenciadorNuvens();
+gerenciadorNuves["padrao"] = new NuvemConcreta("branco", "azul");
+gerenciadorNuves["personalizada"] = new NuvemConcreta("branco", "laranja");
+
+NuvemConcreta um = gerenciadorNuves["padrao"].Clone() as NuvemConcreta;
+NuvemConcreta dois = gerenciadorNuves["padrao"].Clone() as NuvemConcreta;
+NuvemConcreta tres = gerenciadorNuves["personalizada"].Clone() as NuvemConcreta;
+
+Console.ReadKey();
