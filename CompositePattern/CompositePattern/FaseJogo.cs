f98c26c422c1950df03662c0c44b004860f8b169
Gean Anjos <ganjos@kornerstonecredit.com>
2022-09-26 22:50:05 -0300

    feat: Exemplo do Composite Pattern

---
+++ CompositePattern/CompositePattern/FaseJogo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    public class FaseJogo : ComponenteFase
+    {
+        public FaseJogo(string nome) : base(nome)
+        {
+
+        
+        }
+
+        public override void Adicionar(ComponenteFase c)
+        {
+            Console.WriteLine("Nao e possivel adicionar a fase no jogo por aqui");
+        }
+
+        public override void Mostrar(int profundidade)
+        {
+            Console.WriteLine(new string('-', profundidade) + nome);
+        }
+
+        public override void Remover(ComponenteFase c)
+        {
+            Console.WriteLine("Nao e possivel remover a fase no jogo por aqui");
+        }
+    }
+}
---
+++ CompositePattern/CompositePattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using CompositePattern;
+
+Composite mapa = new Composite("MAPA DAS CAVERNAS");
+
+Composite caverna1 = new Composite("Caverna 1");
+caverna1.Adicionar(new FaseJogo("Sub Fase 1"));
+caverna1.Adicionar(new FaseJogo("Sub Fase 2"));
+caverna1.Adicionar(new FaseJogo("Sub Fase 3"));
+
+Composite caverna2 = new Composite("Caverna 2");
+caverna2.Adicionar(new FaseJogo("Sub Fase 4"));
+caverna2.Adicionar(new FaseJogo("Sub Fase 5"));
+
+Composite porta_secreta = new Composite("Porta secreta");
+porta_secreta.Adicionar(new FaseJogo("Sub Fase Secreta X"));
+
+mapa.Adicionar(caverna1);
+mapa.Adicionar(caverna2);
+caverna2.Adicionar(porta_secreta);
+
+mapa.Mostrar(1);
+
+Console.ReadKey();
