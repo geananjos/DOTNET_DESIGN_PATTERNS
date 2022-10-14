26437d41d10de10f90767ee29369377f73fd9e1b
Gean Anjos <ganjos@kornerstonecredit.com>
2022-10-13 23:47:06 -0300

    feat: Exemplo do Chain of Responsability Pattern

---
+++ ChainOfResponsabilityPattern/ChainOfResponsabilityPattern/PersonagemA.cs
@@ -0,0 +1,13 @@
+﻿namespace ChainOfResponsabilityPattern
+{
+    public class PersonagemA : Manipulador
+    {
+        public override void Convoca(int quantidadePoder)
+        {
+            if (quantidadePoder >= 0 && quantidadePoder < 10)
+                Console.WriteLine("{0} convocado para uma força de poder de {1}", this.GetType().Name, quantidadePoder);
+            else if(sucessor != null)
+                sucessor.Convoca(quantidadePoder);
+        }
+    }
+}
---
+++ ChainOfResponsabilityPattern/ChainOfResponsabilityPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using ChainOfResponsabilityPattern;
+
+Manipulador pA = new PersonagemA();
+Manipulador pB = new PersonagemB();
+Manipulador pC = new PersonagemC();
+
+pA.defineSucessor(pB);
+pB.defineSucessor(pC);
+
+int[] requisicoes = { 5, 8, 15, 20, 18, 3, 27, 20};
+
+foreach (int req in requisicoes)
+{
+    pA.Convoca(req);
+}
+
+Console.ReadKey();
